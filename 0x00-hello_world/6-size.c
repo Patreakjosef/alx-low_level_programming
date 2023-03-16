c7d00a3e5150028b646f3ef056922d6429380f57
Patreakjosef <patrickuchechukwujoseph10@gmail.com>
2023-03-16 16:41:20 -0700

    fu

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	char i;
+	int n;
+	long long int y;
+	long long int z;
+	float f;
+	printf("Size of a char: %lu \n", sizeof(i));
+	printf("Size of a in: %lu. \n", sizeof(n));
+	printf("Size of a long int: %lu. \n", sizeof(y));
+	printf("Size of a long long int: %lu. \n", sizeof(z));
+	printf("Size of a float: %lu. \n", sizeof(f));
+	return (0);
+}
