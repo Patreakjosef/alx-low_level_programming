2098088061330c6e07def113d704ce4d98120325
Patreakjosef <patrickuchechukwujoseph10@gmail.com>
2023-03-17 14:40:06 -0700

    d

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - prints alpha in lower case
+*Return: Always 0
+*/
+int main(void)
+{
+	char alpha;
+	alpha == 'a';
+	while ('a' < 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	};
+	return (0);
+}
