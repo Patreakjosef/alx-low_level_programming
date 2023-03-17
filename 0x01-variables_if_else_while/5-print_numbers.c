92af4982d615d5333966550d20b259d4c825d899
Patreakjosef <patrickuchechukwujoseph10@gmail.com>
2023-03-17 15:39:56 -0700

    dd

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - prints alpha in lower case
+*Return: Always 0
+*/
+int main(void)
+{
+	int alpha;
+
+	for (alpha = 0; alpha <= 10; alpha++)
+	{
+		printf("%d\n",alpha);
+	};
+	return (0);
+}
