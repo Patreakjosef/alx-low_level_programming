aa6603e058be84b58dca92bfc179d622c534608c
Patreakjosef <patrickuchechukwujoseph10@gmail.com>
2023-03-21 16:54:40 -0700

    ds

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Computes the absolute value of an integer
+ * @n: The integer to be computed
+ * Return: The absolute value
+ */
+int print_last_digit(int n)
+{
+	int ld = n % 10;
+}
