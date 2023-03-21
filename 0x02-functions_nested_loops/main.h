50238ed14fbc9a6dac71df391aa3f4976d05fa45
Patreakjosef <patrickuchechukwujoseph10@gmail.com>
2023-03-21 14:48:45 -0700

    s

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
-void print_alphabet(void)
-int _putchar(char c)
+#include <stdio.h>;
+#include <stdlib.h>;
+void print_alphabet(void);
+int _putchar(char c)'
 #endif
