3cad56ee77d1001770c280484bb129a555ebe708
Patreakjosef <patrickuchechukwujoseph10@gmail.com>
2023-03-21 17:52:00 -0700

    ee

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,9 +6,10 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int print_last_digit(int);
+int print_last_digit(intg;
 void times_table(void);
 void jack_bauer(void);
+void times_table(void);
 void print_alphabet_x10(void);
 int _putchar(char c);
 #endif
