b45337fd865f87e94c2033f70ff72368cf0b9ea3
Patreakjosef <patrickuchechukwujoseph10@gmail.com>
2023-03-27 16:05:11 -0700

    dd

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - swaping values
+ * @a: first int
+ * @b: second int
+ * Return - return swap_int
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
